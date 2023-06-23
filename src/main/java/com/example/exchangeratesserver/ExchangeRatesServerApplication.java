f03c7f84631f1df8fbad627014b5ad05e3cabe3f
alekseytumbaev <leha.tumbaev@gmail.com>
2023-06-23 20:38:19 +0700

    initial commit

---
+++ src/main/java/com/example/exchangeratesserver/ExchangeRatesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchangeratesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exchangeratesserver/ExchangeRatesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangeratesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
