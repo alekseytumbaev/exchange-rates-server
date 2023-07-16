ab8d7f82dca9950bd090dcfbf4241ccf672a9d70
alekseytumbaev <leha.tumbaev@gmail.com>
2023-07-16 18:53:41 +0700

    Исправлено: cors для подключения по веб сокетам, тестовое уведомление теперь приходит в том же формате, что и обычное

--- src/main/java/com/example/exchangeratesserver/config/WebSocketConfig.java
+++ src/main/java/com/example/exchangeratesserver/config/WebSocketConfig.java
@@ -21,6 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(StompPaths.WEBSOCKETS).withSockJS();
+        registry.addEndpoint(StompPaths.WEBSOCKETS).setAllowedOriginPatterns("**").withSockJS();
     }
 }
